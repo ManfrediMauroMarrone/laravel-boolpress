09c08b58de4719ed48a437bdd9cdab3d3b09f03b
Manfredi Mauro Marrone <manfredi.marrone@hotmail.it>
2021-01-28 16:29:03 +0100

    creato relazione tabelle

--- app/Category.php
+++ app/Category.php
@@ -6,5 +6,8 @@ use Illuminate\Database\Eloquent\Model;
 
 class Category extends Model
 {
-    //
+  // metto post al plurale perché le categorie possono riferirsi a più post
+  public function posts(){
+    return $this->hasMany('App\Post');
+  }
 }
